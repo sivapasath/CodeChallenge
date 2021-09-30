9194838f18f6cf98defa9332effbe9484f3f3288
SivaprasathMani <sivaprasathmani@gmail.com>
2021-09-30 10:47:25 +0530

    TestCase Added

--- app/src/test/java/com/tcs/codechalllenge/ui/SearchListViewModelTest.kt
+++ app/src/test/java/com/tcs/codechalllenge/ui/SearchListViewModelTest.kt
@@ -1,13 +1,15 @@
 package com.tcs.codechalllenge.ui
 
+import com.tcs.codechalllenge.model.SearchListResponse
 import com.tcs.codechalllenge.services.ServiceRepository
+import junit.framework.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.mockito.Mock
-import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
+import retrofit2.Response
 
 @RunWith(JUnit4::class)
 class SearchListViewModelTest {
@@ -24,12 +26,12 @@ class SearchListViewModelTest {
     }
 
     @Test
-    fun getSearchList() {
-       `when`(searchListViewModel.getSearchList("hmm")).thenReturn(any())
+    fun getSearchListNotEmpty() {
+        val rawResponse: Response<List<SearchListResponse>> =
+            serviceRepository.getSearchList("hmm").execute()
 
-        searchListViewModel.getSearchList("hmm")
-
-        verify(searchListViewModel.getSearchList("hmm"))
+        assertTrue(rawResponse.isSuccessful)
+        rawResponse.body()?.let { assertTrue(it.isNotEmpty()) }
     }
 }
 
